91355a72590849e4bef5f47a60a0f87a837f5962
徐子崴 <eric03330eric@gmail.com>
2018-02-15 14:30:28 +0800

    no message

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 var nav = require('./components/nav.js');
-AppRegistry.registerComponent('SpeedLoan', () => App);
+AppRegistry.registerComponent('SpeedLoan-x', () => App);
